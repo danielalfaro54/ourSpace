2a38d5d8dae746382987f61a9d0ca1fb9c018837
Daniel Alfaro <a216203326@unison.mx>
2021-06-15 21:20:56 -0700

    F

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const requireLogin = require("../middleware/requireLogin");
+const Post = mongoose.model("Post")
 
-router.post("/createpost",requireLogin,(req,res)=>{
+router.post('/createpost',requireLogin,(req,res)=>{
     const {title,body} = req.body;
     if(!title||!body){
        return res.status(422).json({error:'please fill all the fields'})
     }
-    console.log(req.user)
-    res.send('OK') 
-    // const post = new post({
-    //     title,
-    //     body,
-        
-    // })
-})
+    //req.user.password = undefined
+     const post = new Post({
+         title,
+         body,
+         postedby:req.user
+     })
+     post.save().then(result=>{
+         res.json({post:result})
+     })
+     .catch(err=>{
+         console.log(err);
+     })
+});
 module.exports = router
