ff6d965269327c3b135e9b38cbb3bcc8f20db795
Daniel Alfaro <a216203326@unison.mx>
2021-06-22 17:38:05 -0700

    displaying allpost and myposts

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,6 +4,16 @@ const mongoose = require("mongoose");
 const requireLogin = require("../middleware/requireLogin");
 const Post = mongoose.model("Post")
 
+router.get("/allpost",(req,res)=>{
+    Post.find()
+    .populate("postedby","_id name")
+    .then(posts=>{
+        res.json(posts)
+    }).catch(err=>{
+        console.log(err);
+    })
+})
+
 router.post('/createpost',requireLogin,(req,res)=>{
     const {title,body} = req.body;
     if(!title||!body){
@@ -22,4 +32,16 @@ router.post('/createpost',requireLogin,(req,res)=>{
          console.log(err);
      })
 });
+
+router.get("/mypost", requireLogin,(req,res)=>{
+    Post.find({postedby:req.user._id})
+    .populate("postedby","_id name")
+    .then(mypost=>{
+        res.json({mypost})
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+})
+
 module.exports = router
