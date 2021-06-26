a266f366ce0a1cb75b5b548b028afd650af20b7d
Daniel Alfaro <a216203326@unison.mx>
2021-06-25 19:43:20 -0700

    server side signIn

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,7 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+//import M from 'materialize-css'
 
 export const Signin= () => {
+    const history = useHistory("")
+    const [email, setemail] = useState("")
+    const [password, setpassword] = useState()
+    const PostData = ()=>{
+        //verifiyng email format
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+            M.toast({html:"Correo electrónico inválido"})
+            return
+        }
+        fetch("/signin",{
+        method:"post",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(
+            {
+                email:email,
+                password:password
+            }
+        )
+    }).then(res=>res.json()).then(data=>{
+        if(data.error){
+            M.toast({html:data.error})
+        }else{
+            M.toast({html:"Ingresó correctamente"})
+            history.push("/")
+        }
+    }).catch(err=>{
+        console.log(err)
+    })
+    }
     return (
         <div className='mycard'>
         <div className="card auth-card">
@@ -9,13 +41,23 @@ export const Signin= () => {
            <input
            type='text'
            placeholder='Correo electrónico'
+           value= {email}
+           onChange = {(e)=>{
+                setemail(e.target.value)
+           }}
            />
            <input
            type='text'
            placeholder='Contraseña'
+           value= {password}
+           onChange = {(e)=>{
+                setpassword(e.target.value)
+           }}
            />
             <button className="btn waves-effect waves-light" type="submit" name="action">Login
-    <i className="material-icons right">send</i>
+    <i className="material-icons right"
+    onClick= {()=>PostData()}
+    >send</i>
         </button>
         <h6><Link to='/signup'>No tengo una cuenta
         </Link></h6>
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,7 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import M from 'materialize-css'
 
 export const Signup= () => {
+    const history = useHistory("")
+    const [name, setname] = useState("")
+    const [email, setemail] = useState("")
+    const [password, setpassword] = useState()
+    const PostData = ()=>{
+        //verifiyng email format
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+            M.toast({html:"Correo electrónico inválido"})
+            return
+        }
+        fetch("/signup",{
+        method:"post",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(
+            {
+                name:name,
+                email:email,
+                password:password
+            }
+        )
+    }).then(res=>res.json()).then(data=>{
+        if(data.error){
+            M.toast({html:data.error})
+        }else{
+            M.toast({html:data.message})
+            history.push("/signin")
+        }
+    }).catch(err=>{
+        console.log(err)
+    })
+    }
     return (
         <div className='mycard'>
         <div className="card auth-card">
@@ -9,16 +43,32 @@ export const Signup= () => {
            <input
            type='text'
            placeholder='Nombre'
+           value= {name}
+           onChange = {(e)=>{
+                setname(e.target.value)
+           }}
            />
            <input
            type='text'
            placeholder='Correo electrónico'
+           value= {email}
+           onChange = {(e)=>{
+                setemail(e.target.value)
+           }}
            />
            <input
            type='text'
            placeholder='Contraseña'
+           value= {password}
+           onChange = {(e)=>{
+                setpassword(e.target.value)
+           }}
            />
-            <button className="btn waves-effect waves-light" type="submit" name="action">Login
+            <button 
+            onClick={()=>{
+                PostData()
+            }}
+            className="btn waves-effect waves-light" type="submit" name="action">Sign up
     <i className="material-icons right">send</i>
         </button>
         <h6><Link to='/signin'>Ya tienes una cuenta?
