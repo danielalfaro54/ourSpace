a81e27a78b7377ff25023b2636b3df5ccda87b18
daniel <jdalfaro98@gmail.com>
2021-08-17 19:38:12 -0700

    nav-bar 'cerrarSesion' li

--- server/client/src/components/Navbar.js
+++ server/client/src/components/Navbar.js
@@ -9,17 +9,14 @@ const Navbar= ()=>{
     if(state){
       return [
         <>
-          <li><Link to="/create">Publicar</Link></li>
-          <li><Link to="/profile">Mi Perfil</Link></li>
-          <li><Link to="/myfollowingposts">Siguiendo</Link></li>
-          <li>
-          <button className="btn waves-effect waves-light #" type="submit" name="action" 
-          onClick={()=>{
-              localStorage.clear()
-              dispatch({type:"CLEAR"})
-              history.push("/signin")
-          }}>Cerrar sesión
-        </button>
+          <li><Link to="/create" className='links'>Publicar</Link></li>
+          <li><Link to="/profile" className='links'>Mi Perfil</Link></li>
+          <li><Link to="/myfollowingposts" className='links'>Siguiendo</Link></li>
+          <li><Link className='links' 
+           onClick={()=>{
+            localStorage.clear()
+            dispatch({type:"CLEAR"})
+            history.push("/signin")}}>Cerrar sesión</Link>
           </li>
         </>
       ]
