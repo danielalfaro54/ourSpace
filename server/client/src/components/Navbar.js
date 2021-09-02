2b16af2140b52df2afafff5c790d9397c70f2eb8
daniel <jdalfaro98@gmail.com>
2021-09-01 18:35:20 -0700

    navbarLinks+

--- server/client/src/components/Navbar.js
+++ server/client/src/components/Navbar.js
@@ -11,7 +11,6 @@ const Navbar= ()=>{
         <>
           <li><Link to="/create" className='links'>Publicar</Link></li>
           <li><Link to="/profile" className='links'>Mi Perfil</Link></li>
-          <li><Link to="/followingposts" className='links'>Siguiendo</Link></li>
           <li><Link className='links' 
            onClick={()=>{
             localStorage.clear()
